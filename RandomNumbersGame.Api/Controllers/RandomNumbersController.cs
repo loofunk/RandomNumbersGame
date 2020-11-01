73fa73968bf14d53454d1fc5e936a11c41bb547e
Louis <louis.dasouki@carfinance247.co.uk>
2020-11-01 09:24:29 +0000

    Added Unit Tests, created API controller
    Added basic unit tests
    Created API controller scaffold

---
+++ RandomNumbersGame.Api/Controllers/RandomNumbersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RandomNumbersGame.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandomNumbersController : ControllerBase
+    {        
+        private readonly ILogger<RandomNumbersController> _logger;
+
+        public RandomNumbersController(ILogger<RandomNumbersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<int> Get()
+        {
+            return 0;
+        }
+    }
+}
--- RandomNumbersGame.Tests/UnitTest1.cs
+++ RandomNumbersGame.Tests/UnitTest1.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 
 namespace RandomNumbersGame.Tests
 {
-    public class Tests
+    public class RandomNumbersTests
     {
         [SetUp]
         public void Setup()
@@ -10,9 +10,45 @@ namespace RandomNumbersGame.Tests
         }
 
         [Test]
-        public void Test1()
+        public void ShouldGenerateARandomNumberBetween1and100()
         {
-            Assert.Pass();
+            Assert.Fail();
+        }
+
+        [Test]
+        public void ShouldCorrectlyStateIsHigherThanInput(int input)
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void ShouldCorrectlyStateIsLowerThanInput(int input)
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void ShouldEnsureGeneratedNumberIsNotEqualPreviouslyGeneratedNumber(int previousGeneratedNumber)
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void ShouldCorrectlyShowIfCorrectInput(int input)
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void GivenInputShouldReturnCorrectAmountOfPoints(int input)
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void GivenMaximumPointsSetOfPointsShouldSetGameToFinished(int input)
+        {
+            Assert.Fail();
         }
     }
 }
